c6df2a82faecf8ae651bc97d0cde9c8f65388aa1
Matthew Lloyd <Matt.Lloyd101@gmail.com>
2020-10-18 20:42:37 +0100

    Tidy tests

--- test/constraints/DisjunctionConstraint.test.ts
+++ test/constraints/DisjunctionConstraint.test.ts
@@ -3,12 +3,15 @@ import {SUDOKU_DOMAIN} from "../Constants";
 
 describe("DisjunctionConstraint", () => {
    test.each([
-       [SUDOKU_DOMAIN, [1], [2, 3, 4, 5, 6, 7, 8, 9]]
+       [SUDOKU_DOMAIN, [1], [2, 3, 4, 5, 6, 7, 8, 9]],
+       [SUDOKU_DOMAIN, [1, 2, 3, 9], [4, 5, 6, 7, 8]],
+       [SUDOKU_DOMAIN, [1, 2, 3, 4, 5, 6, 7, 8, 9], []],
+       [SUDOKU_DOMAIN, [1, 2, 3, 10], [4, 5, 6, 7, 8, 9]],
+       [SUDOKU_DOMAIN, [], [1, 2, 3, 4, 5, 6, 7, 8, 9]],
    ])("It returns the disjunction between the domain and the constraint", (domainValues, values, expected) => {
-       const disjunctionConstraint = new DisjunctionConstraint(new Set(values));
-
+       const constraint = new DisjunctionConstraint(new Set(values));
        const domain = new Set(domainValues);
-       const actual = disjunctionConstraint.apply(domain);
+       const actual = constraint.apply(domain);
 
        expect(actual).not.toBeUndefined();
        expect(actual).not.toBeNull();
--- test/constraints/IntersectionConstraint.test.ts
+++ test/constraints/IntersectionConstraint.test.ts
@@ -5,6 +5,7 @@ describe("IntersectionConstraint", () => {
 
    test.each([
        [SUDOKU_DOMAIN, [1], [1]],
+       [SUDOKU_DOMAIN, [1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9]],
        [SUDOKU_DOMAIN, [1, 2, 3], [1, 2, 3]],
        [SUDOKU_DOMAIN, [1, 2, 3, 10], [1, 2, 3]],
        [SUDOKU_DOMAIN, [10], []],
